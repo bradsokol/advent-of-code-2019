c3f7b6d2347cde98addc85210546650059003531
Brad Sokol <bsokol@gmail.com>
2019-12-01 07:46:06 -0500

    Day 1

---
+++ day1.rb
@@ -0,0 +1,19 @@
+#! /usr/bin/env ruby
+
+# frozen_string_literal: true
+
+modules = File.open('day1.txt').readlines.map(&:to_i)
+
+puts modules.inject(0) { |memo, m| memo + ((m / 3) - 2) }
+
+def calculate_fuel(m)
+  fuel = (m / 3) - 2
+  if fuel > 0
+    fuel += calculate_fuel(fuel)
+  else
+    fuel = 0
+  end
+  fuel
+end
+
+puts modules.inject(0) { |memo, m| memo += calculate_fuel(m) }
