3571d959785e30c70800068c784f028fd842f90b
Brad Sokol <bsokol@gmail.com>
2019-12-11 16:22:07 -0500

    Day 11 part 2

---
+++ day11-2.rb
@@ -0,0 +1,177 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'pry'
+
+# code = $stdin.readline.split(',').map(&:to_i)
+code = File.open('day11.txt').readline.split(',').map(&:to_i)
+
+class Computer
+  def initialize(code)
+    @inputs = []
+    @halted = false
+    @ip = 0
+    @relative_base = 0
+
+    @code = Hash.new(0)
+    code.each_with_index { |value, i| @code[i] = value }
+  end
+
+  def compute(input)
+    @inputs << input
+    while @code[@ip] != 99
+      case @code[@ip] % 100
+      when 1
+        value1, value2, store = parse3(@ip)
+        write_value(@ip + 3, value1 + value2, @code[@ip] / 10000)
+        @ip += 4
+      when 2
+        value1, value2, store = parse3(@ip)
+        write_value(@ip + 3, value1 * value2, @code[@ip] / 10000)
+        @ip += 4
+      when 3
+        if @inputs.empty?
+          puts "No inputs at ip #{@ip}"
+          exit(1)
+        end
+        write_value(@ip + 1, @inputs.shift, @code[@ip] / 100)
+        @ip += 2
+      when 4
+        value = read_value(@ip + 1, @code[@ip] / 100)
+        yield value
+        @ip += 2
+      when 5
+        value1, value2 = parse2(@ip)
+        @ip = (value1 != 0) ? value2 : (@ip + 3)
+      when 6
+        value1, value2 = parse2(@ip)
+        @ip = (value1 == 0) ? value2 : (@ip + 3)
+      when 7
+        value1, value2, store = parse3(@ip)
+        write_value(@ip + 3, (value1 < value2) ? 1 : 0, @code[@ip] / 10000)
+        @ip += 4
+      when 8
+        value1, value2, store = parse3(@ip)
+        write_value(@ip + 3, (value1 == value2) ? 1 : 0, @code[@ip] / 10000)
+        @ip += 4
+      when 9
+        @relative_base += read_value(@ip + 1, @code[@ip] / 100)
+        @ip += 2
+      else
+        puts "Bad opcode #{@code[@ip]} at #{@ip}"
+        exit(1)
+      end
+    end
+    @halted = true
+  end
+
+  def halted?
+    @halted
+  end
+
+  def add_input(input)
+    @inputs << input
+  end
+
+  private
+
+  def read_value(location, parameter_mode)
+    value = @code[location]
+    case parameter_mode
+    when 0
+      @code[value]
+    when 1
+      value
+    when 2
+      @code[@relative_base + value]
+    else
+      puts "Bad parameter mode #{parameter_mode}"
+      exit(1)
+    end
+  end
+
+  def write_value(location, value, parameter_mode)
+    final_location = @code[location]
+    case parameter_mode
+    when 0
+      @code[final_location] = value
+    when 2
+      final_location += @relative_base
+      if final_location < 0
+        puts "Invalid address #{final_location}"
+        exit(1)
+      end
+      @code[final_location] = value
+    else
+      puts "Unsupported parameter mode #{parameter_mode}"
+      exit(1)
+    end
+  end
+
+  def parse2(ip)
+    op_code = @code[ip]
+
+    parameter_mode = op_code % 1000 / 100
+    value1 = read_value(ip + 1, parameter_mode)
+    parameter_mode = op_code % 10000 / 1000
+    value2 = read_value(ip + 2, parameter_mode)
+    [value1, value2]
+  end
+
+  def parse3(ip)
+    parse2(ip) << @code[ip + 3]
+  end
+end
+
+def move(direction, x, y)
+  case direction
+  when 0
+    [x, y - 1]
+  when 1
+    [x + 1, y]
+  when 2
+    [x, y + 1]
+  when 3
+    [x - 1, y]
+  else
+    puts "Bad direction #{direction}"
+    exit(1)
+  end
+end
+
+grid = Hash.new(0)
+x = 0
+y = 0
+direction = 0
+robot = Computer.new(code)
+state = 0
+
+robot.compute(1) do |output|
+  if state == 0
+    grid[[x, y]] = output
+    state = 1
+  else
+    if output == 0
+      direction -= 1
+    else
+      direction += 1
+    end
+    direction %= 4
+    x, y = move(direction, x, y)
+    robot.add_input(grid[[x, y]])
+    state = 0
+  end
+end
+
+min_x = grid.keys.map(&:first).min
+max_x = grid.keys.map(&:first).max
+min_y = grid.keys.map { |panel| panel[1] }.min
+max_y = grid.keys.map { |panel| panel[1] }.max
+
+(min_y..max_y).each do |y|
+  (min_x..max_x).each do |x|
+    print grid[[x, y]] == 0 ? ' ' : 'X'
+  end
+  print "\n"
+  $stdout.flush
+end
