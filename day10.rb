6c0fa2d7233d57e850144571e6ea914e6fda9c3b
Brad Sokol <bsokol@gmail.com>
2019-12-11 11:14:23 -0500

    Day 10 part 1 refactor

--- day10.rb
+++ day10.rb
@@ -3,54 +3,33 @@
 
 require 'set'
 
-def count_asteroids(station_x, station_y)
-  vectors = Set.new
-  ((station_x + 1)...$grid[station_y].length).each do |x|
-    (0..station_y).each do |y|
-      next if x == station_x && y == station_y
-      vectors.add((station_x - x.to_f) / (station_y - y.to_f)) if $grid[y][x] == '#'
-    end
-  end
-  sum = vectors.length
+def count_asteroids(grid, station_x, station_y)
+  ranges = [
+    [((station_x + 1)...grid[station_y].length), (0..station_y)],
+    [(station_x...grid[station_y].length), ((station_y + 1)...grid.length)],
+    [(0...station_x),(station_y...grid.length)],
+    [(0..station_x), (0...station_y)],
+  ]
 
-  vectors = Set.new
-  (station_x...$grid[station_y].length).each do |x|
-    ((station_y + 1)...$grid.length).each do |y|
-      next if x == station_x && y == station_y
-      vectors.add((station_x - x.to_f) / (station_y - y.to_f)) if $grid[y][x] == '#'
-    end
-  end
-  sum += vectors.length
+  ranges.reduce(0) do |sum, ranges|
+    vectors = Set.new
+    ranges[0].each do |x|
+      ranges[1].each do |y|
+        next if x == station_x && y == station_y
 
-  vectors = Set.new
-  (0...station_x).each do |x|
-    (station_y...$grid.length).each do |y|
-      next if x == station_x && y == station_y
-      vectors.add((station_x - x.to_f) / (station_y - y.to_f)) if $grid[y][x] == '#'
+        vectors.add((station_x - x.to_f) / (station_y - y.to_f)) if grid[y][x] == '#'
+      end
     end
+    sum + vectors.length
   end
-  sum += vectors.length
-
-  vectors = Set.new
-  (0..station_x).each do |x|
-    (0...station_y).each do |y|
-      next if x == station_x && y == station_y
-      vectors.add((station_x - x.to_f) / (station_y - y.to_f)) if $grid[y][x] == '#'
-    end
-  end
-  sum += vectors.length
-
-  sum
 end
 
-$grid = $stdin.readlines.map { |line| line.strip.chars }.compact
+grid = $stdin.readlines.map { |line| line.strip.chars }.compact
 
 max = 0
-$grid.each_with_index do |row, y|
+grid.each_with_index do |row, y|
   row.each_with_index do |location, x|
-    if location == '#'
-      max = [count_asteroids(x, y), max].max
-    end
+    max = [count_asteroids(grid, x, y), max].max if location == '#'
   end
 end
 
