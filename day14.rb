8c4c45ea3c4d628c872f23e2763cca3a0bb1cf18
Brad Sokol <bsokol@gmail.com>
2019-12-14 14:39:59 -0500

    Day 14 part 1

---
+++ day14.rb
@@ -0,0 +1,63 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'pry'
+
+class Chemical
+  attr_reader :amount, :name
+
+  def initialize(text)
+    @amount, @name = text.scan(/(\d+) ([A-Z]+)/).first
+    @amount = @amount.to_i
+  end
+
+  def to_s
+    "#{amount} #{name}"
+  end
+end
+
+class Reaction
+  attr_reader :inputs, :output
+
+  def initialize(inputs, output)
+    @output = Chemical.new(output)
+    @inputs = inputs.split(', ').map { |input| Chemical.new(input) }
+  end
+end
+
+class Reactor
+  def initialize(reactions)
+    @reactions = reactions
+    @products = Hash.new(0)
+  end
+
+  def perform
+    react(@reactions['FUEL'], 0)
+  end
+
+  private
+
+  def react(reaction, ores)
+    reaction.inputs.each do |input|
+      if input.name == 'ORE'
+        ores += input.amount
+      else
+        while (input.amount - @products[input.name]) > 0
+          ores = react(@reactions[input.name], ores)
+        end
+        @products[input.name] -= input.amount
+      end
+    end
+    @products[reaction.output.name] += reaction.output.amount
+    ores
+  end
+end
+
+reactions = {}
+# $stdin.readlines.each do |line|
+File.open('day14.txt').readlines.each do |line|
+  inputs, outputs = line.scan(/(.*) => (.*)/).first
+  reaction = Reaction.new(inputs, outputs)
+  reactions[reaction.output.name] = reaction
+end
+puts Reactor.new(reactions).perform
