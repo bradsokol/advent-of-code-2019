eae51882d9736162f522d0e006c199dfdf4abd0e
Brad Sokol <bsokol@gmail.com>
2019-12-16 06:34:41 -0500

    Progress on Day 15 part 1

---
+++ day15.rb
@@ -0,0 +1,104 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'pry'
+
+require_relative './computer.rb'
+
+code = File.open('day15.txt').readline.split(',').map(&:to_i)
+
+class Step
+  attr_reader :direction
+
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @direction = 1
+  end
+
+  def location
+    [@x, @y]
+  end
+
+  def next_direction
+    @direction = if @direction == 0
+      @direction
+    else
+      (@direction + 1) % 5
+    end
+  end
+end
+
+class Robot
+  def initialize(code)
+    @computer = Computer.new(code.dup)
+    @path = [Step.new(0, 0)]
+    @map = Hash.new('?')
+    @map[@path.last.location] = '.'
+  end
+
+  def explore
+    @computer.compute(self) do |output|
+      puts @path.length
+      @map[@path.last.location] = case output
+      when 0
+        '#'
+      when 1
+        '.'
+      when 2
+        puts @path.length
+        exit(0)
+        'O'
+      end
+    end
+  end
+
+  def next_input
+    next_step
+    @path.last.direction
+  end
+
+  def print_map
+    min_x = @map.keys.map { |key| key[0] }.min
+    max_x = @map.keys.map { |key| key[0] }.max
+    min_y = @map.keys.map { |key| key[1] }.min
+    max_y = @map.keys.map { |key| key[1] }.max
+
+    (min_y..max_y).each do |y|
+      (min_x..max_x).each do |x|
+        print @map[[x, y]]
+      end
+      puts "\n"
+    end
+  end
+
+  private
+
+  def next_step
+    while true
+      next_direction = @path.last.direction
+      # puts "#{next_direction} #{@x} #{@y} #{@path.length}"
+      if next_direction == 0
+        @path.pop
+      else
+        x, y = @path.last.location
+        new_location = case next_direction
+        when 1
+          [x, y + 1]
+        when 2
+          [x, y - 1]
+        when 3
+          [x - 1, y]
+        when 4
+          [x + 1, y]
+        end
+        @path.last.next_direction
+        next if @map.key?(new_location)
+        @path.push(Step.new(new_location[0], new_location[1]))
+        break
+      end
+    end
+  end
+end
+
+Robot.new(code).explore
